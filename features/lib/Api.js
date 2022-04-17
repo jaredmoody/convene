631d6eb4a25f26c384dee925c74df78ae4e09e9b
Zee Spencer <zspencer@users.noreply.github.com>
2022-04-17 15:25:06 -0700

    These aren't quite there yet

--- features/lib/Api.js
+++ features/lib/Api.js
@@ -3,8 +3,6 @@ const { pascalCase } = require('change-case');
 const applyCaseMiddleware = require("axios-case-converter").default;
 const Space = require("./Space");
 const AuthenticationMethod = require("./AuthenticationMethod");
-const SpaceMembership = require("./SpaceMembership");
-const { camelCase } = require("lodash");
 class Api {
   constructor(host, apiKey) {
     this.host = host;
@@ -38,17 +36,6 @@ class Api {
       client: this,
       endpoint: "/authentication_methods",
       model: AuthenticationMethod,
-    });
-  }
-
-  /**
-   * @returns {Repository}
-   */
-  spaceMemberships() {
-    return new Repository({
-      client: this,
-      endpoint: "/space_memberships",
-      model: SpaceMembership,
     });
   }
 
